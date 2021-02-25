877ae5acb52a8f218275b1a68c1ed5a003712dac
Pablo Portillo <oswalxtiyo@gmail.com>
2021-02-25 17:20:52 +0000

    Email service nodejs14

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.listen(port, () => {
   console.log('Listening on port', port);
 });
 
+app.get('/', async (req, res) => {
+    console.log(`Email Service: health check`);
+    res.status(200).send('Health check..');
+})
+
 app.post('/', async (req, res) => {
   const notification = decodeBase64Json(req.body.message.data);
   try {
